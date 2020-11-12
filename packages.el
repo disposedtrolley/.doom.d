ae69f262b67cfa92fa92c454a968e83aab93daab
James Liu <james_liu@fastmail.com>
2020-11-12 20:10:43 +1100

    feat: enable spacemacs-light theme

--- packages.el
+++ packages.el
@@ -18,6 +18,7 @@
 (package! git-link)
 (package! graphql-mode)
 (package! org-roam)
+(package! spacemacs-theme)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
