bf5ac753a5dc1133c403dad06bbc9659824e6bc4
James Liu <james@jamesliu.io>
2021-06-26 10:33:48 +1000

    feat: hackernews, gist, ruby, calendar

--- packages.el
+++ packages.el
@@ -14,6 +14,7 @@
 (package! git-link)
 (package! spacemacs-theme)
 (package! zetteldeft)
+(package! hackernews)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
