7f91ed9243136d83b72ccb0de5b1f7a6aa1921b9
James Liu <james.liu@cultureamp.com>
2021-08-06 13:06:08 +1000

    Enable direnv and docker

--- init.el
+++ init.el
@@ -84,8 +84,8 @@
        :tools
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
-       ;;direnv
-       ;;docker
+       direnv
+       docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
