6b7cbc2bd2daef7c793058f88e3ccaa240766f33
James Liu <james@jamesliu.io>
2021-10-23 11:56:13 +1100

    Reorganise configuration

--- config.el
+++ config.el
@@ -1,27 +1,25 @@
 (setq user-full-name "James Liu"
       user-mail-address "james@jamesliu.io")
 
+(setq-default line-spacing 3)
 (setq doom-font (font-spec :family "Hack Nerd Font" :size 15))
+(setq doom-themes-treemacs-enable-variable-pitch nil) ;; use doom-font for treemacs
+
 (setq doom-theme 'modus-vivendi)
+(setq doom-themes-treemacs-theme "doom-atom")
+
 (setq display-line-numbers-type t)
 
-(setq-default line-spacing 2)
-
-(setq doom-themes-treemacs-enable-variable-pitch nil) ;; use doom-font for treemacs
-(setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-(doom-themes-treemacs-config)
-
-(doom-themes-org-config)
-
-(setq +pretty-code-enabled-modes nil)
-
 (setq-default word-wrap t)
-(global-visual-line-mode t)
+(global-visual-line-mode t) ;; navigate lines visually, not logically
 
-;; Integrated titlebar on macOS
-;; (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(setq-default fill-column 80)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
 
 (setq projectile-project-search-path '("~/projects/"))
+
+;; Auth keys.
+(setq auth-sources '("~/.authinfo"))
 
 (with-eval-after-load 'magit
   (require 'forge)
@@ -32,33 +30,36 @@
 (setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-(setq lsp-rust-server 'rust-analyzer)
-(setq rustic-lsp-server 'rust-analyzer)
-
 ;; parinfer-rust-mode doesn't automatically install correctly on Apple M1.
 ;; To address this issue, the library was compiled separately and copied
 ;; to ~/.emacs.d/parinfer-rust/libparinfer_rust.so (notice the extension
 ;; was renamed).
 (setq parinfer-rust-library "~/.emacs.d/parinfer-rust/libparinfer_rust.so")
 
+(setq lsp-rust-server 'rust-analyzer)
+(setq rustic-lsp-server 'rust-analyzer)
+
 (setq org-directory "~/Dropbox/zettel/")
 (setq org-agenda-files (directory-files-recursively "~/Dropbox/zettel/" "\.org$"))
-
 (setq org-default-notes-file "~/Dropbox/zettel/inbox.org")
 
+;; Provides <s shortcut to insert source blocks.
 (require 'org-tempo)
 
-(setq org-src-tab-acts-natively t)
-
-(setq org-src-fontify-natively t
+(setq org-src-tab-acts-natively t
+      org-src-fontify-natively t
       org-src-tab-acts-natively t
       org-confirm-babel-evaluate nil
       org-edit-src-content-indentation 0)
+
+;; Go-specific config
 (require 'ob-go)
 
+;; Clojure-specific config
 (setq org-babel-clojure-backend 'cider)
 (require 'cider)
 
+;; Register other languages for Org Babel
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((ruby . t)
@@ -70,26 +71,11 @@
    (clojure .t)
    (C .t)))
 
-(setq org-capture-templates
-  '(
-     ("t" "Quick task" entry
-     (file "")
-     "* TODO %^{Task}\n"
-     :immediate-finish t)
-    ("T" "Task" entry
-     (file "")
-     "* TODO %^{Task}\n")
-    ("n" "Quick note" entry
-     (file "")
-     "* :NOTE: %^{Note}\n"
-     :immediate-finish t)
-    ("N" "Note" entry
-     (file "")
-     "* :NOTE: %^{Note}\n")))
-
+;; Drag-and-drop images to an Org buffer.
 (require 'org-download)
 (add-hook 'dired-mode-hook 'org-download-enable)
 
+;; Display images inline.
 (setq org-startup-with-inline-images t)
 (setq org-image-actual-width (/ (display-pixel-width) 3))
 
@@ -99,29 +85,9 @@
 (push (org-projectile-project-todo-entry) org-capture-templates)
 (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
 
-(setq-default fill-column 80)
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-
-(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
-
-(defun my-nov-font-setup ()
-  (face-remap-add-relative 'variable-pitch :family "Liberation Serif"
-                                           :height 1.4))
-(add-hook 'nov-mode-hook 'my-nov-font-setup)
-
-(setq nov-text-width 80)
-(setq nov-text-width t)
-(setq visual-fill-column-center-text t)
-(add-hook 'nov-mode-hook 'visual-line-mode)
-(add-hook 'nov-mode-hook 'visual-fill-column-mode)
-
-(setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+(setq deft-directory org-directory)
+;; Enable keybinds.
+(zetteldeft-set-classic-keybindings)
 
 (require 'atomic-chrome)
 (atomic-chrome-start-server)
-
-(setq auth-sources '("~/.authinfo"))
-
-(setq deft-directory "~/Dropbox/zettel")
-;; Enable keybinds.
-(zetteldeft-set-classic-keybindings)
