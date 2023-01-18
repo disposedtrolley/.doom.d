91952a21c66d92e0f38d591b783ea2d8841f51f1
James Liu <james@jamesliu.io>
2023-01-18 14:53:54 +1100

    2023 stuff

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1fab98300b100a19010734a14c4bf9b6712ffc8b9e1d7eca35f837adeeabf740" "986cdc701d133f7c6b596f06ab5c847bebdd53eb6bc5992e90446d2ddff2ad9e" default))
+ '(package-selected-packages '(modus-themes)))
+(custom-set-faces)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+(setq doom-theme 'modus-operandi-tinted)
